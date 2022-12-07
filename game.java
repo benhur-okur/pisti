02bb395ad17ce01e8e09fdb767fa06c53b4cbc22
benhur-okur <117393881+benhur-okur@users.noreply.github.com>
2022-12-07 23:29:40 +0300

    cutted
     ı have cutted the desk and made the bottoms to the up

--- game.java
+++ game.java
@@ -7,7 +7,10 @@ public class game {
 	
 		int k, n;
 
-		String[] deck = new String[52];
+		String[] deckBC = new String[52];
+		String[] deckAC = new String[52];
+
+		int cut;
 
 		for (int i = 0;i < 52; i++) {
 			boolean found = false;
@@ -18,7 +21,7 @@ public class game {
 					found = false;
 					continue;
 				}
-				deck[i] = arr[k][n];
+				deckBC[i] = arr[k][n];
 				arr[k][n] = "_";
 				found = true;
 				
@@ -28,8 +31,57 @@ public class game {
 
 
 		
-		for (int i = 0;i<deck.length;i++) {
+		/*for (int i = 0;i<deckBC.length;i++) {
 			System.out.println(deck[i]);
+		}*/
+
+
+		System.out.println("cutting the deck ...");
+
+		cut = ran.nextInt(52);
+
+		int x = cut;
+
+		int y = cut;
+
+		int z = cut;
+
+		System.out.println("cutting ...");
+		System.out.println("cutted from: " + cut);
+
+		int L = deckBC.length;
+
+
+		for(int i = 0;i<L - 1 - z;i++) {
+			deckAC[i] = deckBC[x + 1];
+			x++;
+		}
+
+		for(int i = 0;i < y + 1;i++) {
+			deckAC[L-1-y] = deckBC[i];
+			L++;
+
+		}
+		System.out.println();
+
+		System.out.println("before cut");
+
+		System.out.println();
+
+
+		for (int i = 0;i<deckBC.length;i++) {
+			System.out.println(deckBC[i]);
+		}
+
+		System.out.println();
+		
+		System.out.println("after cut: ");
+
+		System.out.println();
+
+
+		for (int i = 0;i<deckAC.length;i++) {
+			System.out.println(deckAC[i]);
 		}
 		
 
